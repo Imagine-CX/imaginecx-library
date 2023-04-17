570ee84f6144997024042f81760f6f6eede748f0
Sergio Mejia <sdmejiac@gmail.com>
2023-04-17 12:04:10 -0500

    add event onClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { Button, CheckBox, Dropdown, Header } from './components';
 import { Helloworld } from './components/HelloWorld';
 
+const handleClick = () => {
+  // console.log("Haz hecho clic en el botón");
+};
+
 function App() {
   return (
     <div className="icx-container icx-w-[100vw] icx-mx-2">
@@ -14,10 +18,10 @@ function App() {
         <Button text="Button Primary" color="icx-btn-primary" />
         <Button text="Button Secondary" color="icx-btn-secondary" />
         <Button text="Disable" color="icx-btn-secondary" disabled={true} />
-        <Button text="Alternative" color="icx-btn-alternative" />
+        <Button text="Alternative" color="icx-btn-alternative" handleClick={handleClick} />
       </div>
       <div>
-        <CheckBox text="Checkbox" disabled={true} />
+        <CheckBox text="Checkbox disable" disabled={true} />
         <CheckBox text="Checkbox2" />
       </div>
     </div>
--- src/components/Btns/Button.tsx
+++ src/components/Btns/Button.tsx
@@ -4,13 +4,18 @@ export interface IBtnPrimary {
   text: string;
   color: string;
   disabled?: boolean;
+  handleClick?: () => void;
 }
 
-export const Button = ({ text, color, disabled }: IBtnPrimary): JSX.Element => {
+export const Button = ({ text, color, disabled, handleClick }: IBtnPrimary): JSX.Element => {
   const estiloBtn: string = disabled ? 'icx-btn-disable' : `${color}`;
 
   return (
-    <button className={`${estiloBtn} icx-m-3 icx-px-8 icx-py-2 icx-rounded-full`} disabled={disabled}>
+    <button
+      className={`${estiloBtn} icx-m-3 icx-px-8 icx-py-2 icx-rounded-full`}
+      disabled={disabled}
+      onClick={handleClick}
+    >
       <span>{text}</span>
     </button>
   );
