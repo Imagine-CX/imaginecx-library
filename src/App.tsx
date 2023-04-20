00ca0bc507ed009393c831162ad8edc06edbc039
Sergio Mejia <sdmejiac@gmail.com>
2023-04-20 11:25:40 -0500

    Toggle created and cursor not allowed in disabled fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import { BiCalendarX, BiSearch } from 'react-icons/bi';
+import { BiSearch } from 'react-icons/bi';
 
-import { Button, CheckBox, Header, InputField, Radio } from './components';
-
-const handleClick = () => {
-  // console.log("Haz hecho clic en el botón");
-};
-
-const handleChange = () => {
-  // console.log("Radio button checked");
-};
+import { Button, CheckBox, Header, InputField, Radio, Toggle } from './components';
 
 const options = [
   { id: 1, value: 'option1', label: 'Option 1' },
@@ -58,13 +50,35 @@ function App() {
               name="apellido"
               label="Apellido"
               disabled
-              icon={<BiCalendarX className="icx-w-5 icx-h-5" />}
+              icon={
+                <svg
+                  fill="#e8af11"
+                  width="25px"
+                  height="25px"
+                  viewBox="0 0 32.00 32.00"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="#e8af11"
+                  stroke-width="0.00032"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {' '}
+                    <title>alt-brightness</title>{' '}
+                    <path d="M2.016 16q0 0.832 0.576 1.44t1.408 0.576 1.408-0.576 0.608-1.44-0.608-1.408-1.408-0.576-1.408 0.576-0.576 1.408zM5.504 24.512q0 0.8 0.576 1.408t1.44 0.576 1.408-0.576 0.576-1.408-0.576-1.408-1.408-0.608-1.44 0.608-0.576 1.408zM5.504 7.52q0 0.832 0.576 1.408t1.44 0.576 1.408-0.576 0.576-1.408-0.576-1.408-1.408-0.608-1.44 0.608-0.576 1.408zM8 16q0 3.328 2.336 5.664t5.664 2.336 5.664-2.336 2.336-5.664-2.336-5.632-5.664-2.368-5.664 2.368-2.336 5.632zM12 16q0-1.632 1.184-2.816t2.816-1.184 2.816 1.184 1.184 2.816-1.184 2.848-2.816 1.152-2.816-1.152-1.184-2.848zM14.016 28q0 0.832 0.576 1.44t1.408 0.576 1.408-0.576 0.608-1.44-0.608-1.408-1.408-0.576-1.408 0.576-0.576 1.408zM14.016 4q0 0.832 0.576 1.44t1.408 0.576 1.408-0.576 0.608-1.44-0.608-1.408-1.408-0.576-1.408 0.576-0.576 1.408zM22.496 24.512q0 0.8 0.576 1.408t1.408 0.576 1.44-0.576 0.576-1.408-0.576-1.408-1.44-0.608-1.408 0.608-0.576 1.408zM22.496 7.52q0 0.832 0.576 1.408t1.408 0.576 1.44-0.576 0.576-1.408-0.576-1.408-1.44-0.608-1.408 0.608-0.576 1.408zM26.016 16q0 0.832 0.576 1.44t1.408 0.576 1.408-0.576 0.608-1.44-0.608-1.408-1.408-0.576-1.408 0.576-0.576 1.408z"></path>{' '}
+                  </g>
+                </svg>
+              }
               required
             />
           </div>
         </div>
         <Button text="Subir" color="icx-btn-primary" type="submit" />
       </form>
+      <div>
+        <Toggle text="Toggle" />
+      </div>
     </div>
   );
 }
