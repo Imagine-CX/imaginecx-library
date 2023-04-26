899e3a318134a3187789ec131bb7b2c7ce01da55
Sergio Mejia <sdmejiac@gmail.com>
2023-04-26 12:18:54 -0500

    Add icons in Buttons and styles corrected buttons

--- src/components/Btns/Button.tsx
+++ src/components/Btns/Button.tsx
@@ -5,15 +5,26 @@ import { ButtonHTMLAttributes } from 'react';
 export interface IBtnPrimary extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
   color: string;
+  icon?: JSX.Element | null;
 }
 
-export const Button = ({ text, color, disabled, ...btnProps }: IBtnPrimary): JSX.Element => {
+export const Button = ({ text, color, disabled, icon, ...btnProps }: IBtnPrimary): JSX.Element => {
   const estiloBtn: string = disabled ? 'icx-btn-disable' : `${color}`;
 
   return (
     <>
-      <button className={`${estiloBtn} icx-m-1 icx-px-5 icx-py-2 icx-rounded-xl`} {...btnProps}>
-        <span className="">{text}</span>
+      <button
+        type="button"
+        className={`icx-flex ${estiloBtn} icx-rounded-lg icx-py-2 icx-mb-1 icx-w-full`}
+        disabled={disabled}
+        {...btnProps}
+      >
+        <div className="icx-m-auto">
+          <div className="icx-flex icx-items-center icx-space-x-2">
+            {icon ? <div>{icon}</div> : <div></div>}
+            <span>{text}</span>
+          </div>
+        </div>
       </button>
     </>
   );
