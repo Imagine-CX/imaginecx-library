7795c2ea5757997e193cf902311ba949d04b5cb1
Sergio Mejia <sdmejiac@gmail.com>
2023-04-26 09:59:14 -0500

    Select Month and styles day selected

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,17 +1,17 @@
-import { add, differenceInDays, endOfMonth, format, startOfMonth, sub } from 'date-fns';
+import { add, differenceInDays, endOfMonth, format, setDate, startOfMonth, sub, toDate } from 'date-fns';
 
 import { getDaysWeek, getMonths } from '../helpers';
 import { Cell } from './Cell';
 
 const daysOfWeek = getDaysWeek(6);
-// const months = getMonths(3);
+const months = getMonths(3);
 
 export interface ICalendar extends React.PropsWithChildren {
   value?: Date;
   onChange?: (value: Date) => void;
 }
 
-export const Calendar = ({ value = new Date(), onChange, ...props }: ICalendar): JSX.Element => {
+export const Calendar = ({ value = new Date(), onChange }: ICalendar): JSX.Element => {
   const startDate = startOfMonth(value);
   const endDate = endOfMonth(value);
   const numDays = differenceInDays(endDate, startDate) + 1;
@@ -19,30 +19,44 @@ export const Calendar = ({ value = new Date(), onChange, ...props }: ICalendar):
   const prefixDays = startDate.getDay();
   const suffixDays = 6 - endDate.getDay();
 
+  const handleChangeMonth = (event: any) => {
+    event.preventDefault();
+    const month = event.target.value;
+    const newMonth = toDate(value.setMonth(month));
+    onChange && onChange(newMonth);
+  };
+
   const prevMonth = () => onChange && onChange(sub(value, { months: 1 }));
   const nextMonth = () => onChange && onChange(add(value, { months: 1 }));
   const prevYear = () => onChange && onChange(sub(value, { years: 1 }));
   const nextYear = () => onChange && onChange(add(value, { years: 1 }));
 
+  const handleClickDate = (index: number) => {
+    const date = setDate(value, index);
+    onChange && onChange(date);
+  };
+
   return (
     <div className="icx-w-[400px] icx-border icx-rounded-xl icx-p-8 icx-m-3 icx-drop-shadow-lg">
       <div className="icx-grid icx-grid-cols-7 icx-items-center icx-justify-center icx-text-center">
         <Cell onClick={prevMonth}>{'<'}</Cell>
-        <Cell>{format(value, 'MMMM')}</Cell>
+        {/* <Cell>{format(value, 'MMMM')}</Cell> */}
+        <Cell>
+          <select name="" id="months" onChange={handleChangeMonth}>
+            {months.map((mes, index) => (
+              <option value={index} key={mes}>
+                {' '}
+                {mes}
+              </option>
+            ))}
+          </select>
+        </Cell>
         <Cell onClick={nextMonth}>{'>'}</Cell>
         <Cell />
         <Cell onClick={prevYear}>{'<'}</Cell>
         <Cell>{format(value, 'yyyy')}</Cell>
         <Cell onClick={nextYear}>{'>'}</Cell>
-        {/* <Cell>
-          <select name="" id="months" onChange={ prevMonth }>
-            {
-              months.map( (mes) => (
-                <option value={ mes } key={ mes }> { mes }</option>
-              ))
-            }
-          </select>
-        </Cell> */}
+
         {daysOfWeek.map((day) => (
           <Cell className="icx-font-bold" key={day}>
             {day}
@@ -53,9 +67,15 @@ export const Calendar = ({ value = new Date(), onChange, ...props }: ICalendar):
           <Cell key={index} />
         ))}
 
-        {Array.from({ length: numDays }).map((_, index) => (
-          <Cell key={index + 1}>{index + 1}</Cell>
-        ))}
+        {Array.from({ length: numDays }).map((_, index) => {
+          const date = index + 1;
+          const isCurrentDate = date === value.getDate();
+          return (
+            <Cell isActive={isCurrentDate} onClick={() => handleClickDate(date)} key={date}>
+              {date}
+            </Cell>
+          );
+        })}
 
         {Array.from({ length: suffixDays }).map((_, index) => (
           <Cell key={index} />
--- src/components/Calendar/Cell.tsx
+++ src/components/Calendar/Cell.tsx
@@ -4,17 +4,19 @@ import React from 'react';
 export interface ICell extends React.PropsWithChildren {
   className?: string;
   onClick?: () => void;
+  isActive?: boolean;
 }
 
-export const Cell = ({ className, children, onClick }: ICell): JSX.Element => {
+export const Cell = ({ className, children, isActive = false, onClick }: ICell): JSX.Element => {
   return (
     <div
+      onClick={isActive ? undefined : onClick}
       className={clsx(
         'icx-h-12 icx-flex icx-items-center icx-justify-center icx-cursor-pointer icx-rounded-full',
-        'hover:icx-bg-gray-100 ',
+        { 'icx-text-white icx-bg-primary-400 hover:icx-bg-primary-500': isActive },
+        { 'hover:icx-bg-gray-200 ': !isActive && onClick },
         className,
       )}
-      onClick={onClick}
     >
       {children}
     </div>
