14e5033eab05c9936afce104fd08965032c1c2eb
jfprado11 <juanfeprado11@gmail.com>
2024-12-29 20:44:35 -0500

    updating to vite v6

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { resolve } from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import EsLint from 'vite-plugin-linter';
+import { EsLinter, linterPlugin } from 'vite-plugin-linter';
 import tsConfigPaths from 'vite-tsconfig-paths';
-const { EsLinter, linterPlugin } = EsLint;
+
 import * as packageJson from './package.json';
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
